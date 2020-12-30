7ebfeb163afc19966a232e62a013635d565efcbc
Denilson Costa <denilson2770@gmail.com>
2020-12-30 03:11:23 +0100

    add _app types

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-import '../styles/globals.scss';
+import type { AppProps } from "next/app";
+import "../styles/globals.scss";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
 
-export default MyApp
+export default MyApp;
